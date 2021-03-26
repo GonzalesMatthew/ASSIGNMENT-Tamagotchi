0207ea16ff6117be47147b2f4eb95455197c9703
Matthew Gonzales <gonzalesmattg@gmail.com>
2021-03-25 20:29:15 -0700

    randomized activity points with a random number generator function

--- src/javascripts/helpers/sleepFunctions.js
+++ src/javascripts/helpers/sleepFunctions.js
@@ -1,12 +1,14 @@
+import getRandomArbitrary from './randomNum';
+
 function nap() {
   let energy = Number(document.querySelector('#energy').innerHTML);
-  energy = Math.min(energy + 50, 100);
+  energy = Math.min(energy + getRandomArbitrary(20, 30), 100);
   document.querySelector('#energy').innerHTML = `${energy}`;
 }
 
 function slumber() {
   let energy = Number(document.querySelector('#energy').innerHTML);
-  energy = Math.min(energy + 60, 100);
+  energy = Math.min(energy + getRandomArbitrary(60, 80), 100);
   document.querySelector('#energy').innerHTML = `${energy}`;
 }
 
