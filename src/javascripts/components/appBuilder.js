10d87435f547a7658821a96af112ee80071c90c2
Matthew Gonzales <gonzalesmattg@gmail.com>
2021-03-26 16:26:35 -0700

    fixed picture scaling

--- src/javascripts/components/appBuilder.js
+++ src/javascripts/components/appBuilder.js
@@ -4,9 +4,11 @@ function appBuilder() {
   document.querySelector('#app').classList.add('border', 'border-dark', 'border-5', 'rounded');
   const domString = `
   <div class="d-flex justify-content-center">
-  <button type="button" class="btn btn-primary" id="namePet" data-toggle="collapse" data-target="#formContainer">Name Pet</button>
+    <button type="button" class="btn btn-primary" id="namePet" data-toggle="collapse" data-target="#formContainer">Name Pet</button>
   </div>
+  
   <div id="formContainer" class="d-flex justify-content-center"></div>
+  
   <div id="pet" class="d-flex justify-content-center"></div>
   
   <div id="progress"></div>
