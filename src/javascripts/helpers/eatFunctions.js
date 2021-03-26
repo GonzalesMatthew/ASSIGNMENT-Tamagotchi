44a1e97e8d1b37db96cdec3eb52327884059698e
Matthew Gonzales <gonzalesmattg@gmail.com>
2021-03-25 21:01:41 -0700

    functions affect other scores - more realistic

--- src/javascripts/helpers/eatFunctions.js
+++ src/javascripts/helpers/eatFunctions.js
@@ -2,14 +2,30 @@ import getRandomArbitrary from './randomNum';
 
 function wholeFoods() {
   let full = Number(document.querySelector('#full').innerHTML);
-  full = Math.min(full + getRandomArbitrary(6, 10), 100);
+  full = Math.min(full + getRandomArbitrary(18, 26), 100);
   document.querySelector('#full').innerHTML = `${full}`;
+
+  let strength = Number(document.querySelector('#strength').innerHTML);
+  strength = Math.min(strength + getRandomArbitrary(23, 35), 100);
+  document.querySelector('#strength').innerHTML = `${strength}`;
+
+  let energy = Number(document.querySelector('#energy').innerHTML);
+  energy = Math.max(energy - getRandomArbitrary(8, 11), 0);
+  document.querySelector('#energy').innerHTML = `${energy}`;
 }
 
 function junkFood() {
   let full = Number(document.querySelector('#full').innerHTML);
   full = Math.max(full - getRandomArbitrary(3, 7), 0);
   document.querySelector('#full').innerHTML = `${full}`;
+
+  let fun = Number(document.querySelector('#fun').innerHTML);
+  fun = Math.min(fun + getRandomArbitrary(4, 12), 100);
+  document.querySelector('#fun').innerHTML = `${fun}`;
+
+  let energy = Number(document.querySelector('#energy').innerHTML);
+  energy = Math.max(energy - getRandomArbitrary(6, 10), 0);
+  document.querySelector('#energy').innerHTML = `${energy}`;
 }
 
 export { wholeFoods, junkFood };
--- src/javascripts/helpers/fightFunctions.js
+++ src/javascripts/helpers/fightFunctions.js
@@ -4,11 +4,27 @@ function runAway() {
   let strength = Number(document.querySelector('#strength').innerHTML);
   strength = Math.min(strength + getRandomArbitrary(1, 3), 100);
   document.querySelector('#strength').innerHTML = `${strength}`;
+
+  let energy = Number(document.querySelector('#energy').innerHTML);
+  energy = Math.max(energy - getRandomArbitrary(3, 6), 0);
+  document.querySelector('#energy').innerHTML = `${energy}`;
+
+  let full = Number(document.querySelector('#full').innerHTML);
+  full = Math.max(full - getRandomArbitrary(3, 5), 0);
+  document.querySelector('#full').innerHTML = `${full}`;
 }
 
 function attack() {
   let strength = Number(document.querySelector('#strength').innerHTML);
   strength = Math.max(strength - getRandomArbitrary(7, 12), 0);
   document.querySelector('#strength').innerHTML = `${strength}`;
+
+  let energy = Number(document.querySelector('#energy').innerHTML);
+  energy = Math.max(energy - getRandomArbitrary(10, 20), 0);
+  document.querySelector('#energy').innerHTML = `${energy}`;
+
+  let full = Number(document.querySelector('#full').innerHTML);
+  full = Math.max(full - getRandomArbitrary(8, 15), 0);
+  document.querySelector('#full').innerHTML = `${full}`;
 }
 export { runAway, attack };
